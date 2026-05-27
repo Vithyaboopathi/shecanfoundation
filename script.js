50b5c2ddf9bb6cdb27a5747b6d49be8769ecdf04
Vithyaboopathi <106966313+Vithyaboopathi@users.noreply.github.com>
2026-05-27 15:13:41 +0530

    Files added

---
+++ script.js
@@ -0,0 +1,37 @@
+// Dark Mode
+
+function toggleMode() {
+  document.body.classList.toggle("dark");
+}
+
+// Volunteer Button
+
+document.getElementById("volunteerBtn").addEventListener("click", function () {
+  alert("Thank you for showing interest in volunteering ❤️");
+});
+
+// Counter Animation
+
+const counters = document.querySelectorAll(".counter");
+
+counters.forEach((counter) => {
+  counter.innerText = "0";
+
+  const updateCounter = () => {
+    const target = +counter.getAttribute("data-target");
+
+    const current = +counter.innerText;
+
+    const increment = target / 100;
+
+    if (current < target) {
+      counter.innerText = `${Math.ceil(current + increment)}`;
+
+      setTimeout(updateCounter, 20);
+    } else {
+      counter.innerText = target;
+    }
+  };
+
+  updateCounter();
+});
